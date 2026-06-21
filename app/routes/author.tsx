8a2b347ce3d4bfe68ae9c3131db92da1b3ae4775
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-06-22 01:15:47 +0800

    Consistent styling WIP

--- app/routes/author.tsx
+++ app/routes/author.tsx
@@ -1,12 +1,18 @@
 import { Link, Outlet } from "react-router";
 import type { Route } from "./+types/author";
 import styles from "./author.module.css";
+import SvgIcon, { SvgImageList } from "~/components/SvgIcon/SvgIcon";
 
 export default function AuthorLayout({ params }: Route.ComponentProps) {
   return (
     <>
       <header className={styles.header}>
-        <Link to="/" className={styles.back}>← Scribe Reader</Link>
+        <Link to="/" className={styles.back}>
+          <div className={styles.backIconContainer}>
+            <SvgIcon name={SvgImageList.ArrowLeft} />
+            <SvgIcon name={SvgImageList.ScribeReaderLogo} />
+          </div>
+        </Link>
         <span className={styles.handle}>{params.author}</span>
       </header>
       <Outlet />
